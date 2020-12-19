4dc899f4e9215c6c14149dd503bbf27756fe8b8d
Nitesh Kumar <nitesh.kumar@maybank.com>
2020-12-19 18:11:24 +0800

    make it better

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose'
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send("Hello to dashboard API");
+})
+
+// const CONNECTION_URL = 'mongodb+srv://user_01:test123@cluster0.yjzl3.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Sever is running on port: ${PORT}`)))
+.catch((error)=> console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
