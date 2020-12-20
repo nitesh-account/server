b252003d237157d4291e051eafc96da6fbdc93b4
Nitesh04Singh <nitesh04singh@gmail.com>
2020-12-20 18:17:06 +0800

    Updated randomData controller

--- controllers/randomdata.js
+++ controllers/randomdata.js
@@ -1,29 +1,29 @@
 import RandomData from '../models/randomData.js'
 
-export const getPosts = async (req, res) => {
+export const getRandomData = async (req, res) => {
     try {
         const randomData = await RandomData.find();
 
-        res.status(200).json(randomData);
+        res.status(200).json(randomData.length == 0 ? { data: [] } : randomData[0]);
     } catch (error) {
-        res.status(404).json({ message: error.message})
+        res.status(404).json({ message: error.message })
     }
 }
 
 export const createRandomData = async (req, res) => {
-    //const randomData = req.body();
-    let randomData = {data:[]}
-    const randomDataArray = Array.from({ length: 2 } , () => ({ 
-        field1: randomIntegerInRange(0,10),
-        field2: randomIntegerInRange(0,10),
-        field3: randomIntegerInRange(0,10),
-        field4: randomIntegerInRange(0,10),
-        field5: randomIntegerInRange(0,10),
-        field6: randomIntegerInRange(0,10),
-        field7: randomIntegerInRange(0,10),
-        field8: randomIntegerInRange(0,10),
-        field9: randomIntegerInRange(0,10),
-        field10: randomIntegerInRange(0,10) }))
+    let randomData = { data: [] }
+    const randomDataArray = Array.from({ length: 10000 }, () => ({
+        field1: randomIntegerInRange(0, 10),
+        field2: randomIntegerInRange(0, 10),
+        field3: randomIntegerInRange(0, 10),
+        field4: randomIntegerInRange(0, 10),
+        field5: randomIntegerInRange(0, 10),
+        field6: randomIntegerInRange(0, 10),
+        field7: randomIntegerInRange(0, 10),
+        field8: randomIntegerInRange(0, 10),
+        field9: randomIntegerInRange(0, 10),
+        field10: randomIntegerInRange(0, 10)
+    }))
 
     randomData.data = randomDataArray;
 
@@ -31,47 +31,49 @@ export const createRandomData = async (req, res) => {
     try {
         await RandomData.deleteMany({});
         await newRandomData.save();
-        
+
         res.status(201).json(newRandomData);
     } catch (error) {
-        res.status(409).json({message: error.message});
-    }  
+        res.status(409).json({ message: error.message });
+    }
 }
 
-export const resetRandomData = async (req, res) => {    
+export const resetRandomData = async (req, res) => {
     try {
-        await RandomData.deleteMany({})
-        
-        res.status(200).json({message: 'Rows are deleted successfully'});
+        let emptyResData = { data: [] };
+        const deleteRandomData = await RandomData.deleteMany({})
+
+        res.status(200).json(emptyResData);
     } catch (error) {
-        res.status(409).json({message: error.message});
-    }  
+        res.json({ message: error.message });
+    }
 }
 
-export const deleteRandomData = async (req, res) => {    
+export const deleteRandomData = async (req, res) => {
     try {
-       const updateRandomData = await RandomData.updateMany({}, 
-        { $set: { "data.$[elem].activeIndicator": 'N', "data.$[elem].expiryDate": new Date() }},
-        {arrayFilters: [{"elem.activeIndicator": 'Y'}]}
+        await RandomData.updateMany({},
+            { $set: { "data.$[elem].activeIndicator": 'N', "data.$[elem].expiryDate": new Date() } },
+            { arrayFilters: [{ "elem.activeIndicator": 'Y' }] }
         )
-        
-        res.status(200).json({message: "Rows are updated successfully"});
+
+        const updatedRandomData = await RandomData.find()
+        res.status(200).json(updatedRandomData.length == 0 ? { data: [] } : updatedRandomData[0]);
     } catch (error) {
-        res.status(409).json({message: error.message});
-    }  
+        res.json({ message: error.message });
+    }
 }
 
-export const expireRandomData = async (req, res) => {    
+export const expireRandomData = async (req, res) => {
     try {
-       const updateRandomData = await RandomData.updateMany({}, 
-        { $set: { "data.$[elem].expiryDate": new Date() }},
-        {arrayFilters: [{"elem.expiryDate": null}]}
+        const updateRandomData = await RandomData.updateMany({},
+            { $set: { "data.$[elem].expiryDate": new Date() } },
+            { arrayFilters: [{ "elem.expiryDate": null }] }
         )
-        
-        res.status(200).json({message: "Rows are updated successfully"});
+        const updatedRandomData = await RandomData.find()
+        res.status(200).json(updatedRandomData.length == 0 ? { data: [] } : updatedRandomData[0]);
     } catch (error) {
-        res.status(409).json({message: error.message});
-    }  
+        res.status(409).json({ message: error.message });
+    }
 }
 
 
